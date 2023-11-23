b73aeddae2841875e36cfeae84ded3cefbdedd0e
CodeSandbox <support@codesandbox.io>
2023-11-23 10:42:37 +0000

    Sandbox snapshot commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import * as React from "react";
 import { Typography } from "@mui/material";
+import ClientPage from "../components/ClientPage";
 
 export default function HomePage() {
-  return (
-    <Typography>
-      Material UI example. Please put the code to reproduce the issue in
-      src/app/page.tsx
-    </Typography>
-  );
+  return <ClientPage />;
 }
